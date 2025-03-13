3bbc9862db1951c06489e49badb5dc26619e377d
IvaDeneva <denevaiva79@gmail.com>
2025-03-13 18:37:53 +0200

    add to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: 'public/_redirects', dest: '' }]
+    })
+
+  ],
   server:{
     port:3001,
   },
   define: {
     "window.jQuery": "jQuery",
   },
+  
 })
