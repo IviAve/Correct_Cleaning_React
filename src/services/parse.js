0f3f9d0f1ce40cdb5fc40ce1a9fe402e53f3e4fd
IvaDeneva <denevaiva79@gmail.com>
2025-04-01 18:06:21 +0300

    change for secret keys

--- src/services/parse.js
+++ src/services/parse.js
@@ -1,7 +1,26 @@
+// import Parse from 'parse';
+
+
+// Parse.initialize("Ean7ER6LcWvTWRbXiE8on65F4S4zwWEnBzQ1zPW8", "wnkEUp5EaP0FO5PHZzQC5UCgGqdsmiu0qZCpaVgy");
+// Parse.serverURL = 'https://parseapi.back4app.com/';
+
+// export { Parse }; 
+
+
+// import Parse from 'parse';
+
+
+// Parse.initialize(import.meta.env.VITE_APP_ID, import.meta.env.VITE_JS_KEY);
+// Parse.serverURL = import.meta.env.VITE_SERVER_URL;
+
+// export { Parse };
+
+
 import Parse from 'parse';
 
+if (!Parse.applicationId) {
+    Parse.initialize(import.meta.env.VITE_APP_ID, import.meta.env.VITE_JS_KEY);
+    Parse.serverURL = import.meta.env.VITE_SERVER_URL;
+}
 
-Parse.initialize("Ean7ER6LcWvTWRbXiE8on65F4S4zwWEnBzQ1zPW8", "wnkEUp5EaP0FO5PHZzQC5UCgGqdsmiu0qZCpaVgy");
-Parse.serverURL = 'https://parseapi.back4app.com/';
-
-export { Parse }; 
+export { Parse };
