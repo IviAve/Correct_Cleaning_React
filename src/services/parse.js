c569929b6c89710ea23aa612178609a17fd4dad3
IvaDeneva <denevaiva79@gmail.com>
2025-04-02 11:00:57 +0300

    change readmi file and fix css for contacts

--- src/services/parse.js
+++ src/services/parse.js
@@ -1,19 +1,5 @@
-// import Parse from 'parse';
 
 
-// Parse.initialize("Ean7ER6LcWvTWRbXiE8on65F4S4zwWEnBzQ1zPW8", "wnkEUp5EaP0FO5PHZzQC5UCgGqdsmiu0qZCpaVgy");
-// Parse.serverURL = 'https://parseapi.back4app.com/';
-
-// export { Parse }; 
-
-
-// import Parse from 'parse';
-
-
-// Parse.initialize(import.meta.env.VITE_APP_ID, import.meta.env.VITE_JS_KEY);
-// Parse.serverURL = import.meta.env.VITE_SERVER_URL;
-
-// export { Parse };
 
 
 import Parse from 'parse';
